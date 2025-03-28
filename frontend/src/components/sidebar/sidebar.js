440f20064a0ce677950309da3950f6f60577a4b9
abir_yazaoui <145192358+abiryazaoui@users.noreply.github.com>
2025-03-29 00:21:30 +0100

    history

--- frontend/src/components/sidebar/sidebar.js
+++ frontend/src/components/sidebar/sidebar.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-import { FaTachometerAlt, FaClipboardList, FaCalendarAlt, FaChevronRight, FaChevronDown } from "react-icons/fa";
+import { FaTachometerAlt, FaClipboardList, FaCalendarAlt, FaHistory, FaChevronRight, FaChevronDown, FaTasks } from "react-icons/fa"; // Ajout de l'icône FaTasks
 import { Link } from "react-router-dom";
 import "./sidebar.css";
 
 export default function Sidebar() {
   const [isAuditListOpen, setIsAuditListOpen] = useState(false);
+  const [isDashboardOpen, setIsDashboardOpen] = useState(true);
 
   const toggleAuditList = () => {
     setIsAuditListOpen(!isAuditListOpen);
+  };
+
+  const toggleDashboard = () => {
+    setIsDashboardOpen(!isDashboardOpen);
   };
 
   return (
@@ -20,37 +25,57 @@ export default function Sidebar() {
         <div className="nav-section">
           <h3 className="nav-title">Quick Access</h3>
           <ul>
-            <li>
-              <FaTachometerAlt className="nav-icon" />
-              <span>Dashboard</span>
-              <FaChevronRight className="nav-arrow" />
+            <li className={`${isDashboardOpen ? 'active' : ''}`} onClick={toggleDashboard}>
+              <Link to="/dashboard" className="nav-link">
+                <FaTachometerAlt className="nav-icon" />
+                <span>Dashboard</span>
+                {isDashboardOpen ? (
+                  <FaChevronDown className="nav-arrow" />
+                ) : (
+                  <FaChevronRight className="nav-arrow" />
+                )}
+              </Link>
             </li>
           </ul>
         </div>
-        
+
         <div className="nav-section">
-          <h3 className="nav-title">Services</h3>
-          <ul>
-            <li className={`${isAuditListOpen ? 'active' : ''}`} onClick={toggleAuditList}>
-            <Link to="/audits" className="nav-link">
-              <FaClipboardList className="nav-icon" />
-              <span>Audit List</span>
+        
+          <h3 className="nav-title" onClick={toggleAuditList}>
+            <div className="nav-link">
+              <FaTasks className="nav-icon" /> 
+              <span>Audit Management</span>
               {isAuditListOpen ? (
                 <FaChevronDown className="nav-arrow" />
               ) : (
                 <FaChevronRight className="nav-arrow" />
               )}
-              </Link>
-            </li>
-            {isAuditListOpen && (
+            </div>
+          </h3>
+          
+         
+          {isAuditListOpen && (
+            <ul>
+              <li className="dropdown-item">
+                <Link to="/audits" className="nav-link">
+                  <FaClipboardList className="nav-icon" />
+                  <span>Audit List</span>
+                </Link>
+              </li>
               <li className="dropdown-item">
                 <Link to="/calendar" className="nav-link">
                   <FaCalendarAlt className="nav-icon" />
                   <span>Calendar</span>
                 </Link>
               </li>
-            )}
-          </ul>
+              <li className="dropdown-item">
+                <Link to="/audit-history" className="nav-link">
+                  <FaHistory className="nav-icon" />
+                  <span>Audit History</span>
+                </Link>
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
       
